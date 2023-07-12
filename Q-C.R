86569596e66fd7a61240e41edd34f77d1c2e93a4
Srijan Shekhar <112923519+SrijanShekhar21@users.noreply.github.com>
2023-07-12 11:45:34 +0530

    Update Q-C.R

--- Q-C.R
+++ Q-C.R
@@ -1,25 +1,23 @@
-# Question c)
-
 tennis <- function(prob){
-    scoreA = 0
-    scoreB = 0
-    matches = 0
-    while(scoreA < 3 && scoreB < 3){
-        matches = matches + 1
+    sA = 0
+    sB = 0
+    m = 0
+    while(sA < 3 && sB < 3){
+        m = m + 1
         if (rbinom(1, 1, prob)){
             # 1 indicates a win by player A
-            scoreA = scoreA + 1
+            sA = sA + 1
         }
         else {
-            scoreB = scoreB + 1
+            sB = sB + 1
         }
     }
-    return(matches)
+    return(m)
 }
 
 constProb = 0.70
-matches <- rep(1, times = 1000)
+m <- rep(1, times = 1000)
 for(i in 1: 1000){
-    matches[i] <- tennis(constProb)
+    m[i] <- tennis(constProb)
 } 
-ansc <- mean(matches)
+ansc <- mean(m)
