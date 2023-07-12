c842e4162c075356f600de7afdbb554ee6b49223
Srijan Shekhar <112923519+SrijanShekhar21@users.noreply.github.com>
2023-07-12 11:42:47 +0530

    Update Q-A.R

--- Q-A.R
+++ Q-A.R
@@ -7,14 +7,14 @@ library(rvest)
 url <- read_html("https://www.moneyworks4me.com/best-index/nse-stocks/top-nifty50-companies-list/")
 nodes = html_nodes(url, "table")
 head(nodes)
-table = html_table(nodes, header = 1, trim = FALSE, dec = ".") 
+stocktable = html_table(nodes, header = 1, trim = FALSE, dec = ".") 
 
 
 # table stores a list of a list containing the tables
-class(table)
-class(table[1])
-class(table[1][[1]])
-ans = table[1][[1]]
+class(stocktable)
+class(stocktable[1])
+class(stocktable[1][[1]])
+ans = stocktable[1][[1]]
 
 # tidying unnecessary columns
 ans[1] = NULL
